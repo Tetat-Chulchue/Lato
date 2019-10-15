7cf842a7697ac11a4c4efdc22d8288ee2d038f22
61070078 <61070078@kmitl.ac.th>
2019-10-16 00:47:32 +0700

    Add more error

--- Lato/src/main/java/com/mycompany/lato/test.java
+++ Lato/src/main/java/com/mycompany/lato/test.java
@@ -4,23 +4,46 @@
  * and open the template in the editor.
  */
 package com.mycompany.lato;
+import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.WriteResult;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 /**
  *
  * @author Tanawat Chanhom
  */
 public class test {
-    FileInputStream serviceAccount = new FileInputStream("./lato-744d4-firebase-adminsdk-cjt1k-e7c29ad451.json");
+    public static void main(String[] args) throws FileNotFoundException, IOException, InterruptedException, ExecutionException {
+        FileInputStream serviceAccount = new FileInputStream("lato-744d4-firebase-adminsdk-cjt1k-e7c29ad451.json");
 
-    FirebaseOptions options = new FirebaseOptions.Builder()
-    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-    .setDatabaseUrl("https://lato-744d4.firebaseio.com")
-    .build();
-
-    FirebaseApp.initializeApp(options);
+        FirebaseOptions options = new FirebaseOptions.Builder()
+        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+        .setDatabaseUrl("https://lato-744d4.firebaseio.com")
+        .build();
+        FirebaseApp.initializeApp(options);
+        
+        Firestore db = FirestoreClient.getFirestore();
+        
+        DocumentReference docRef = db.collection("users").document("alovelace");
+        Map<String, Object> data = new HashMap<>();
+        data.put("first", "Ada");
+        data.put("last", "Lovelace");
+        data.put("born", 1815);
+        //asynchronously write data
+        ApiFuture<WriteResult> result = docRef.set(data);
+        // ...
+        // result.get() blocks on response
+        System.out.println("Update time : " + result.get().getUpdateTime());
+    }
 }
