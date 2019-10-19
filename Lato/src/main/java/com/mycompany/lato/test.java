de17d44b0f0ac6ffed89be14e73720ef15f9128b
Tanawat Chanhom <61070078@kmitl.ac.th>
2019-10-19 19:34:31 +0700

    Update test

--- Lato/src/main/java/com/mycompany/lato/test.java
+++ Lato/src/main/java/com/mycompany/lato/test.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package com.mycompany.lato;
+
 import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.DocumentReference;
@@ -19,22 +20,24 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
+
 /**
  *
  * @author Tanawat Chanhom
  */
 public class test {
+
     public static void main(String[] args) throws FileNotFoundException, IOException, InterruptedException, ExecutionException {
         FileInputStream serviceAccount = new FileInputStream("./test.json");
 
         FirebaseOptions options = new FirebaseOptions.Builder()
-        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-        .setDatabaseUrl("https://lato-744d4.firebaseio.com")
-        .build();
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl("https://lato-744d4.firebaseio.com")
+                .build();
         FirebaseApp.initializeApp(options);
-        
+
         Firestore db = FirestoreClient.getFirestore();
-        
+
         DocumentReference docRef = db.collection("users").document("alovelace");
         Map<String, Object> data = new HashMap<>();
         data.put("first", "Ada");
