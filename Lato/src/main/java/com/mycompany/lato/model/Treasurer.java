dbdd41fd188912083b60b2d76659be65e36961ff
Tetat <tetat.chulchue@gmail.com>
2019-11-07 22:35:15 +0700

    Add auth code

--- Lato/src/main/java/com/mycompany/lato/model/Treasurer.java
+++ Lato/src/main/java/com/mycompany/lato/model/Treasurer.java
@@ -1,11 +1,21 @@
 package com.mycompany.lato.model;
 
+import java.io.IOException;
+
 public class Treasurer {
     protected String name;
     protected String lastname;
     protected String studentId;
 
-    public Boolean login() {
-        return null;
+    public void login(String email, String password) {
+        String payload = "{\"email\":\"" + email + "\",\"" + "password\":" + password + "\"}";
+        System.out.println(payload);
+        String command = "curl -X POST https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[AIzaSyCNnfqtppF_25UEmRc_ezyHX4IdyauoR0c] --data " + payload + "\"";
+        try {
+            Process process = Runtime.getRuntime().exec(command);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     };
 }
