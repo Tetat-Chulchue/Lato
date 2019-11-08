0c71c887d13d839c84bafaaaa725ef8853043224
Tanawat Chanhom <61070078@kmitl.ac.th>
2019-11-08 22:44:33 +0700

    Update TreasurerLogin UI

--- Lato/src/main/java/UI/TreasurerLogin.java
+++ Lato/src/main/java/UI/TreasurerLogin.java
@@ -4,33 +4,46 @@ import java.awt.*;
 import javax.swing.*;
 public class TreasurerLogin {
     private JFrame fr;
-    private JPanel P1, P2, P3;
+    private JPanel P1, P2, P3, boxContainer, boxUser, boxPass;
     private JButton btn1;
     private JTextField TF1, TF2;
-    private Label LB1;
+    private Label LB1, userText, passText;
 
     public void init() {
-        fr = new JFrame("Login");
+        fr = new JFrame("TreasurerLogin");
         P1 = new JPanel();
         P2 = new JPanel();
         P3 = new JPanel();
-        TF1 = new JTextField("Username");
-        TF2 = new JTextField("Password");
+        boxContainer = new JPanel();
+        boxUser = new JPanel();
+        boxPass = new JPanel();
+        TF1 = new JTextField();
+        TF2 = new JPasswordField();
         LB1 = new Label("LATO");
+        userText = new Label("Username");
+        passText = new Label("Password");
         btn1 = new JButton("Login");
 
         P1.setLayout(null);
         P1.add(LB1);
-        P1.setBackground(Color.green);
+        P1.setBackground(new Color(39, 70, 68));
 
         P2.setLayout(null);
-        P2.add(TF1);
-        P2.add(TF2);
-        P2.setBackground(Color.blue);
+        boxContainer.setLayout(new GridLayout(2, 1));
+        boxUser.setLayout(new GridLayout(2, 1));
+        boxPass.setLayout(new GridLayout(2, 1));
+        boxUser.add(userText);
+        boxUser.add(TF1);
+        boxPass.add(passText);
+        boxPass.add(TF2);
+        boxContainer.add(boxUser);
+        boxContainer.add(boxPass);
+        P2.add(boxContainer);
+        P2.setBackground(new Color(39, 70, 68));
 
         P3.setLayout(null);
         P3.add(btn1);
-        P3.setBackground(Color.red);
+        P3.setBackground(new Color(39, 70, 68));
 
         fr.setLayout(new GridLayout(3, 1));
         fr.add(P1);
@@ -43,20 +56,30 @@ public class TreasurerLogin {
         fr.setResizable(false);
         fr.pack();
 
+//        Style --------------------------->
 
-        LB1.setBounds(0, 0, 110, 40);
-        LB1.setLocation(169, 120);
-        LB1.setFont(new Font("Serif", Font.PLAIN, 40));
-        LB1.setBackground(Color.red);
+        LB1.setBounds(0, 0, 188, 60);
+        LB1.setLocation((fr.getWidth()/2)-(LB1.getWidth()/2), 120);
+        LB1.setFont(new Font("SansSerif", Font.PLAIN, 70));
+        LB1.setForeground(new Color(255, 203, 155));
 
         btn1.setBounds(0, 0, 112, 39);
-        btn1.setLocation(169, 97);
+        btn1.setLocation((fr.getWidth()/2)-(btn1.getWidth()/2), 97);
+        btn1.setBackground(Color.red);
 
-        TF1.setBounds(0, 0, 280, 36);
-        TF1.setLocation(89, 80);
+        boxContainer.setBounds(0, 0, 280, 130);
+        boxContainer.setLocation((fr.getWidth()/2)-(boxContainer.getWidth()/2), 80);
+        boxContainer.setBackground(new Color(39, 70, 68));
 
-        TF2.setBounds(0, 0, 280, 36);
-        TF2.setLocation(89, 130);
+        boxUser.setBackground(new Color(39, 70, 68));
+        boxPass.setBackground(new Color(39, 70, 68));
+
+        userText.setBackground(new Color(39, 70, 68));
+        userText.setForeground(new Color(255, 203, 155));
+        passText.setBackground(new Color(39, 70, 68));
+        passText.setForeground(new Color(255, 203, 155));
+
+//        Style --------------------------->
     }
 
     public static void main(String[] args) {
