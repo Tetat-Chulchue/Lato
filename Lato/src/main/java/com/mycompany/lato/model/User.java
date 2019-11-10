60fbfa188b713ae6dbfdc8eed7f6594694153aee
Tetat <tetat.chulchue@gmail.com>
2019-11-11 02:43:25 +0700

    Update Class User

--- Lato/src/main/java/com/mycompany/lato/model/User.java
+++ Lato/src/main/java/com/mycompany/lato/model/User.java
@@ -1,10 +1,16 @@
 package com.mycompany.lato.model;
 
 public class User {
+    private String name;
+    private String lastname;
+    private String id;
     private Double debt;
 
-    public User(String username, String password, Double debt) {
-        this.debt = debt;
+    public User(String name, String lastname, String id) {
+        this.name = name;
+        this.lastname = lastname;
+        this.id = id;
+        this.debt = 0.0;
     }
 
     public void payDebt(float amount) {
