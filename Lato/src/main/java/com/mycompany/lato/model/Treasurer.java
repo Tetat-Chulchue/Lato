904d2326ae9aca13001f30679d7465f43dd20c73
laxezice <61070048@kmitl.ac.th>
2019-11-18 22:24:55 +0700

    add gson login

--- Lato/src/main/java/com/mycompany/lato/model/Treasurer.java
+++ Lato/src/main/java/com/mycompany/lato/model/Treasurer.java
@@ -1,5 +1,7 @@
 package com.mycompany.lato.model;
 
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -9,7 +11,7 @@ public class Treasurer {
     protected String lastname;
     protected String studentId;
 
-    public void login(String email, String password) {
+    public static boolean login(String email, String password) {
         String payload = "\"email=" + email + "&password=" + password + "\"";
         System.out.println(payload);
         String command = "curl -X POST https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCNnfqtppF_25UEmRc_ezyHX4IdyauoR0c -H 'Content-Type: application/json' --data "+payload;
@@ -18,13 +20,12 @@ public class Treasurer {
             Process process = Runtime.getRuntime().exec(command);
             process.waitFor();
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line = "";
-            while ((line = reader.readLine())!= null)
-            {
-                System.out.println(line);
-            }
+            Gson gson = new Gson();
+            Response res = gson.fromJson(reader, Response.class);
+            return res.isRegistered();
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
+            return  false;
         }
 
     };
